1d014b00a78887ee16f33c36baf6b322d2def33e
Abel <aoun.abel@outlook.com>
2017-06-26 22:36:18 +0200

    add parameter to use a specific storage strategy

--- tpEnegie.py
+++ tpEnegie.py
@@ -1,79 +1,46 @@
-import os, Measure, Appliance, psycopg2
-
-
-# Get the second word (index 1) and remove the
-def get_second_word(line):
-    words = line.split(" : ")
-    return words[1][:-2]
-
-
-def read_appliance(filename):
-    file = open(filename, "r")
-    project_line = file.readline()
-    project = get_second_word(project_line)
-    household_line = file.readline()
-    household = get_second_word(household_line)
-    appliance_line = file.readline()
-    appliance = get_second_word(appliance_line)
-    appliance = Appliance.Appliance(filename, project, household, appliance)
-    file.readline()
-    file.readline()
-    for line in file:
-        slitted_value = line.split("\t")
-        measure = Measure.Measure(slitted_value[0], slitted_value[1],
-                                  slitted_value[2], slitted_value[3])
-        appliance.measures.append(measure)
-        #  print(measure.date + " " + measure.time + " --> "
-        #       + measure.state + " " + measure.energy)
-    return appliance
-
-
-def read_all_appliance():
-    appliances = []
-    dir = './data/'
-    for filename in os.listdir(dir):
-        print(filename)
-        appliance = read_appliance(dir + filename)
-        appliances.append(appliance)
-    return appliances
-
-
-def connect():
-    return psycopg2.connect(host="localhost", database="energie", user="postgres", password="postgres")
-
-
-def insert_appliance(conn, appliance):
-    # print("insertion into DB Energie")
-    appliance_statement = """INSERT INTO appliance(name,project,household,file)
-                             VALUES (%s,%s,%s,%s) RETURNING id"""
-
-    measure_statement = """INSERT INTO measure(appliance,date,state,energy)
-                           VALUES (%s,%s,%s,%s) RETURNING id"""
-
-    cursor = conn.cursor()
-    print(appliance)
-    cursor.execute(appliance_statement, (appliance.name, appliance.project, appliance.household, appliance.filename))
-    app_id = cursor.fetchone()[0]
-    if app_id is not None:
-        for measure in appliance.measures:
-            cursor.execute(measure_statement,
-                           (app_id, measure.date + ":" + measure.time, measure.state, measure.energy))
-        conn.commit()
-    cursor.close()
-
-
-def insert_appliances(appliances):
-    conn = connect()
-    for appliance in appliances:
-        insert_appliance(conn, appliance)
-    conn.close()
-
+import timeit, Parsor, Repository, sys
 
 # #### MAIN #### #
-# applianceTest = read_appliance("data_example/sample.txt")
+
+
+# applianceTest = Parsor.read_appliance("data_example/sample.txt")
 # connTest = connect()
 # insert_appliance(connTest, applianceTest)
 # connTest.close()
 
-appliances_test = read_all_appliance()
-insert_appliances(appliances_test)
+param = sys.argv[1]
+
+
+def execute(strat):
+    start_time_reading = timeit.default_timer()
+    appliances = Parsor.read_all_appliance(strat)
+    reading_elapsed = timeit.default_timer() - start_time_reading
+
+    start_time_inserting = timeit.default_timer()
+    Repository.insert_appliances(appliances, strat)
+    inserting_elapsed = timeit.default_timer() - start_time_inserting
+
+    print("Reading file time : " + str(reading_elapsed) + " s")
+    print("inserting values time : " + str(inserting_elapsed) + " s")
+
+    print("total time : " + str(reading_elapsed + inserting_elapsed) + " s")
+
+    return reading_elapsed + inserting_elapsed
+
+
+if param == '1':
+    execute("SIMPLE")
+elif param == '2':
+    execute("NO_ZERO")
+elif param == '3':
+    execute("FILTERED")
+elif param == '4':
+    execute("SIMPLE")
+    execute("NO_ZERO")
+    execute("FILTERED")
+else:
+    print("Parameter required: ")
+    print("1 - naive storage ")
+    print("2 - no zero values storage ")
+    print("3 - compressed storage")
+    print("4 - every strategy")
